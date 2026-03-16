aa75afd3e1a54da7c6aae02a235186b67a50a198
AnanyaDahal <dahalananya@gmail.com>
2026-03-16 21:35:27 +0545

    managing the routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,45 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Dashboard from "./pages/Dashboard";
-import History from "./pages/History";
-import Report from "./pages/Report";
-import Login from "./pages/login"; 
-import Signup from "./pages/signup"; 
-import ForgotPassword from "./pages/forgotpassword"; 
+// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import Dashboard from "./pages/Dashboard";
+// import History from "./pages/History";
+// import Report from "./pages/Report";
+// import Login from "./pages/login"; 
+// import Signup from "./pages/signup"; 
+// import ForgotPassword from "./pages/forgotpassword"; 
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+        
+//         <Route path="/" element={<Navigate to="/login" />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        
+//         <Route element={<Layout />}>
+//           <Route path="/dashboard" element={<Dashboard />} />
+//           <Route path="/history" element={<History />} />
+//           <Route path="/report/:id" element={<Report />} />
+//         </Route>
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-
-        
-        <Route element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/report/:id" element={<Report />} />
-        </Route>
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 }
