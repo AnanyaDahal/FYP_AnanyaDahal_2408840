f4944c24b92440d23df896cac0d8ed6a7671cf79
AnanyaDahal <dahalananya@gmail.com>
2026-03-16 21:33:34 +0545

    made changes in protectedRoutes page

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,15 +1,19 @@
 import { Navigate } from "react-router-dom";
 
-export const ProtectedRoute = ({ children, adminOnly = false }) => {
+const ProtectedRoute = ({ children, role }) => {
   const user = JSON.parse(localStorage.getItem("user"));
 
+  // Not logged in
   if (!user) {
     return <Navigate to="/login" />;
   }
 
-  if (adminOnly && user.role !== "admin") {
-    return <Navigate to="/dashboard" />; // Send unauthorized users to their dashboard
+  // Role check (for admin routes later)
+  if (role && user.role !== role) {
+    return <Navigate to="/dashboard" />;
   }
 
   return children;
 };
+
+export default ProtectedRoute;
