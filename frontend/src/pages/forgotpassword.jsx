f9440305ef89c57ed30ee183cde9ac898b754be5
AnanyaDahal <dahalananya@gmail.com>
2026-03-18 12:31:14 +0545

    adding forget password in frontend

--- frontend/src/pages/forgotpassword.jsx
+++ frontend/src/pages/forgotpassword.jsx
@@ -15,7 +15,7 @@ const ForgotPassword = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/forgot-password", {
+      const response = await fetch("http://localhost:5000/api/auth/forgot-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
