ffeb397564264fde12c8b60911a0c7f1ec47c2e0
AnanyaDahal <dahalananya@gmail.com>
2026-03-24 09:24:11 +0545

    removed unnecessary functions from emailController

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -1,28 +1,4 @@
 
-
-// const { analyzeEmail } = require("../utils/emailChecks");
-
-// exports.checkEmail = (req, res) => {
-//     const { emailText } = req.body;
-
-//     if (!emailText || emailText.trim() === "") {
-//         return res.status(400).json({
-//             success: false,
-//             message: "Email content is required."
-//         });
-//     }
-
-//     const analysis = analyzeEmail(emailText);
-
-//     res.json({
-//         success: true,
-//         totalUrls: analysis.totalUrls,
-//         analysis: analysis.results
-//     });
-// };
-
-
-// backend/controllers/emailController.js
 const { analyzeEmail } = require("../utils/emailChecks");
 const fs = require("fs");
 const path = require("path");
